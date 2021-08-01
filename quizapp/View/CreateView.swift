47e9833ca18116398f5b09903a9427ad8cad6c74
Sthefanny Gonzaga <sthefannygonzaga@gmail.com>
2021-08-01 15:18:25 -0300

    Create quiz UI done

--- quizapp/View/CreateView.swift
+++ quizapp/View/CreateView.swift
@@ -8,23 +8,116 @@
 import SwiftUI
 
 struct CreateView: View {
+    @StateObject var viewModel = ViewModel()
     @State private var username: String = ""
     
     var body: some View {
         VStack(alignment: .leading) {
-            HStack {
-                Text("Nome do Quiz")
-                    .font(.headline)
-                TextField("Nome do Quiz", text: $username)
+            HStack() {
+                Spacer()
+                VStack() {
+                    imageView(for: viewModel.selectedImage)
+                    imageControlBar()
+                }
+                .sheet(isPresented: $viewModel.isPresentingImagePicker, content: {
+                    ImagePicker(sourceType: viewModel.sourceType, onImagePicked: viewModel.didSelectImage)
+                })
+                Spacer()
             }
             
             HStack {
-                Text("Nome do Quiz")
+                Text("Nome")
                     .font(.headline)
                 TextField("Nome do Quiz", text: $username)
             }
+            .padding(.vertical, 50)
+            
+            HStack {
+                Text("Descrição")
+                    .font(.headline)
+                TextField("Descrição do Quiz", text: $username)
+            }
+            Spacer()
+            
+            pageControlBar()
         }
         .padding(.horizontal, 20)
+    }
+    
+    @ViewBuilder
+    func imageView(for image: UIImage?) -> some View {
+        if let image = image {
+            Image(uiImage: image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300, alignment: .center)
+        }
+        else {
+            Image(systemName: "photo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300, alignment: .center)
+        }
+    }
+    
+    func imageControlBar() -> some View {
+        HStack(spacing: 20) {
+            Button(action: viewModel.choosePhoto, label: {
+                Image(systemName: "photo.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32)
+                    .foregroundColor(.black)
+            })
+            Button(action: viewModel.takePhoto, label: {
+                Image(systemName: "camera.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32)
+                    .foregroundColor(.black)
+            })
+        }.padding()
+    }
+    
+    func pageControlBar() -> some View {
+        HStack() {
+            Spacer()
+            Button(action: {
+                    print("Hello World tapped!")
+                }) {
+                    Text("Criar Perguntas")
+            }
+            Spacer()
+            Button(action: {
+                    print("Hello World tapped!")
+                }) {
+                    Text("Criar Resultados")
+            }
+            Spacer()
+        }.padding()
+    }
+}
+
+extension CreateView {
+    final class ViewModel: ObservableObject {
+        @Published var selectedImage: UIImage?
+        @Published var isPresentingImagePicker = false
+        private(set) var sourceType: ImagePicker.SourceType = .camera
+        
+        func choosePhoto() {
+            sourceType = .photoLibrary
+            isPresentingImagePicker = true
+        }
+        
+        func takePhoto() {
+            sourceType = .camera
+            isPresentingImagePicker = true
+        }
+        
+        func didSelectImage(_ image: UIImage?) {
+            selectedImage = image
+            isPresentingImagePicker = false
+        }
     }
 }
 
